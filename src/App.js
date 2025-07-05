8dc8e72b237ded44e5c04996e769f94ae5a0de6f
Artem M <artemmuntianu@gmail.com>
2025-07-05 16:48:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Picture() {
+  const [isImgActive, setIsImgActive] = useState(false);
+
+  return (
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsImgActive(false);
+      }}
+      className={`background ${isImgActive ? "" : "background--active"}`}
+    >
+      <img
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsImgActive(true);
+        }}
+        className={`picture ${isImgActive ? "picture--active" : ""}`}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+      />
+    </div>
+  );
+}
